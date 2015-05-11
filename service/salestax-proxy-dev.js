7c91b33e342d5b23e9745d044bae78b138664985
Richard Rodger <richard@ricebridge.com>
2015-05-11 03:32:23 +0100

    ie

--- service/salestax-proxy-dev.js
+++ service/salestax-proxy-dev.js
@@ -3,5 +3,6 @@ var PORT = process.env.PORT || 51003
 require('seneca')()
   .client( {pin:'role:salestax,cmd:calculate',port:51001})
   .client( {pin:'role:salestax,cmd:calculate,country:*',port:51002})
+  .client( {pin:'role:salestax,cmd:calculate,country:ie',port:51004})
   .listen( PORT )
 
